83ba3a778854290000e0bc40b58e3335e7c59a89
barakacanbyte <barakajimmy@outlook.com>
2024-08-09 11:11:54 +0300

    changed from private

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/barakajimmy-portfolio/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,
